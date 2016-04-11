940d8ec6f4ff9d89d7f00ef7ad97012042fc8042
Aleksandra Tsvetkova <aleksandra.tsvetkova@intel.com>
2016-04-11 15:26:17 +0300

    Bubblesort algorithm added

--- common.c
+++ common.c
@@ -54,7 +54,7 @@ const Instr_t Primes[PROGRAM_SIZE] = {
     /* print_prime: */
     Instr_Over,          // nmax, c, divisor, c
     Instr_Print,         // nmax, c, divisor
-    /* not_prime */
+    /* not_prime: */
     Instr_Drop,          // nmax, c
     Instr_Inc,           // nmax, c+1
     Instr_Jump, -28, /* back */   // nmax, c
@@ -89,23 +89,93 @@ const Instr_t Newton[PROGRAM_SIZE] = {
     Instr_Push, 1,      // d, n, x, | nx, 1                  26
     Instr_Push, 0,      // d, n, x, | nx, 1, 0               28
     Instr_Set,          // 1, n, x, | nx                     30
-    /* continue */
+    /* continue: */
     Instr_Swap,         // d, n, nx, x                       31
     Instr_Drop,         // d, n, nx                          32
     Instr_Jump, -29, /* loop */ // d, n, nx                  33
-    /* stop */
+    /* stop: */
     Instr_Push, 0,      // d, n, x, | nx, 0                  35
     Instr_Get,          // d, n, x, | nx, d                  37
     Instr_JNE, +2, /* end */ // d, n, x, | nx                38
     Instr_Jump, -11, /* continue */     // d, n, x, | nx     40
-    /* end */
+    /* end: */
     Instr_Drop,         // d, n, x                           42
     Instr_Print,        // d, n                              43
     Instr_Halt          //                                   44
 };
 
+/* Program for sorting an array with bubblesort */
+const Instr_t Bubble[PROGRAM_SIZE] = {
+    Instr_Push, 24,     // N                                 0
+    Instr_Push, 1,      // N, i                              2
+    Instr_Push, 3,      // N, i, j                           4
+    Instr_Push, 0,      // N, i, j, 0                        6
+    Instr_Get,          // N, i, j, N                        8
+    /* gen_num: */
+    Instr_Dec,          // N, i, j, a0..ax, N-1              9
+    Instr_Rand,         // N, i, j, a0..ax, N-1, a[x+1]      10
+    Instr_Swap,         // N, i, j, a0..a[x+1], N-1          11
+    Instr_Dup,          // N, i, j, a0..a[x+1], N-1, N-1     12
+    Instr_JNE, -6, /* get_num */ // N, i, j, a0..a[x+1], N-1 13
+    Instr_Drop,         // N, i, j, a0..a[N-1]               15
+    /* start: */
+    Instr_Push, 2,      // N, i, j, a0..a[N-1], | 2               16
+    Instr_Get,          // .. | j                                 18
+    Instr_Dup,          // .. | j, j                              19
+    Instr_Get,          // .. | j, a[j-3]                         20
+    Instr_Over,         // .. | j, a[j-3], j                      21
+    Instr_Inc,          // .. | j, a[j-3], j+1                    22
+    Instr_Get,          // .. | j, a[j-3], a[j-2]                 23
+    Instr_Over,         // .. | j, a[j-3], a[j-2], a[j-3]         24
+    Instr_Over,         // .. | j, a[j-3], a[j-2], a[j-3], a[j-2] 25
+    Instr_Greater,      // .. | j, a[j-3], a[j-2], a]j-2]>a[j-3]  26
+    Instr_JE, +1, /* no_swap */ // .. | j, a[j-3], a[j-2]         27
+    Instr_Swap,         // .. | j, a[j-2], a[j-3]                 29
+    /* no_swap: */
+    Instr_Push, 2,      // .. | j, a[j-3], a[j-2], 2         30
+    Instr_Get,          // .. | j, a[j-3], a[j-2], j         32
+    Instr_Inc,          // .. | j, a[j-3], a[j-2], j+1       33
+    Instr_Set,          // .. | j, a[j-3]                    34
+    Instr_Over,         // .. | j, a[j-3], j                 35
+    Instr_Set,          // N, i, j, a0..a[N-1], | j          36
+    Instr_Inc,          // N, i, j, a0..a[N-1], | j+1        37
+    Instr_Push, 2,      // N, i, j, a0..a[N-1], | j+1, 2     38
+    Instr_Set,          // N, i, j+1, a0..a[N-1] |           40
+    Instr_Push, 0,      // N, i, j+1, a0..a[N-1] | 0         41
+    Instr_Get,          // N, i, j+1, a0..a[N-1] | N         43
+    Instr_Inc,          // N, i, j+1, a0..a[N-1] | N+1       44
+    Instr_Push, 2,      // N, i, j+1, a0..a[N-1] | N+1, 2    45
+    Instr_Get,          // N, i, j+1, a0..a[N-1] | N+1, j+1  47
+    Instr_Greater,      // N, i, j+1, a0..a[N-1] | j+1>N+1   48
+    Instr_JE, -35, /* start */ // N, i, j+1, a0..a[N-1] |    49
+    Instr_Push, 1,      // N, i, j+1, a0..a[N-1] | 1         51
+    Instr_Get,          // N, i, j+1, a0..a[N-1] | i         53
+    Instr_Inc,          // N, i, j+1, a0..a[N-1] | i+1       54
+    Instr_Push, 1,      // N, i, j+1, a0..a[N-1] | j+1 1     55
+    Instr_Set,          // N, i+1, j+1, a0..a[N-1] |         57
+    Instr_Push, 3,      // N, i+1, j+1, a0..a[N-1] | 3       58
+    Instr_Push, 2,      // N, i+1, j+1, a0..a[N-1] | 3, 2    60
+    Instr_Set,          // N, i+1, j=3, a0..a[N-1] |         62
+    Instr_Push, 0,      // N, i+1, j=3, a0..a[N-1] | 0       63
+    Instr_Get,          // N, i+1, j=3, a0..a{N-1] | N       65
+    Instr_Push, 1,      // N, i+1, j=3, a0..a[N-1] | N, 1    66
+    Instr_Get,          // N, i+1, j=3, a0..a[N-1] | N, i+1  68
+    Instr_Greater,      // N, i+1, j=3, a0..a[N-1] | N<i+1   69
+    Instr_JE, -56, /* start */ // N, i+1, j=3, a0..a[N-1] |  70
+    
+    Instr_Push, 0,      // N, i, j, a0..a[N-1] | 0           72
+    Instr_Get,          // N, i, j, a0..a[N-1] | N           74
+    /* print: */
+    Instr_Dec,          // N, i, j, a0..a[N-x] | N-x         75
+    Instr_Swap,         // N, i, j, a0..a[N-x-1], N-x,a[N-x] 76
+    Instr_Print,        // N, i, j, a0..a[N-x-1], N-x        77
+    Instr_Dup,          // N, i, j, a0..a[N-x-1], N-x, N-x   78
+    Instr_JNE, -6, /* print */ // N, i, j, N-x               79
+    Instr_Halt,         // N, i, j, 0                        81
+};
+
 /* Choose a program we are about to simulate */
-const Instr_t* Program = Newton;
+const Instr_t* Program = Primes;
 
 /* Other programs, kept here just for reference */
 const Instr_t OldProgram[PROGRAM_SIZE] = {
